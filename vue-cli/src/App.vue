260b004b4dd24d6c2c6ebd3fe248495737261bea
jaehyun <vljh246v@naver.com>
2020-09-08 00:33:05 +0900

    vue 첫번째 강의 마지막 수업

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-header v-on:renew="renewStr" v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      str: "Header"
+    };
+  },
   components: {
-    HelloWorld
+    "app-header": AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = "hi";
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id : </label>
+        <input type="text" id="username" v-model="username" />
+      </div>
+      <div>
+        <label for="password">pw : </label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      let url = "https://jsonplaceholder.typicode.com/users";
+      let data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
